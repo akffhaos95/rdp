2d340844b217ed7488dcb4f40faba432b146cfa6
goeunleee <goeun3501@gmail.com>
2024-10-30 17:24:25 +0900

    [fix]dependency 수정

--- src/components/achievement/batter.tsx
+++ src/components/achievement/batter.tsx
@@ -1,21 +1,39 @@
 import { ReactComponent as RightBatter } from "./icons/rightBatter.svg";
 import { ReactComponent as LeftBatter } from "./icons/leftBatter.svg";
-import { side } from "./achivement.style";
-/** @jsxImportSource @emotion/react */
+import styled from "styled-components";
+
+export const SideSingle = styled.div`
+  width: 55px;
+  height: 55px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 5px;
+`;
+
+export const SideDouble = styled.div`
+  width: 110px;
+  height: 55px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 5px;
+`;
+
 type BattingSide = "좌타" | "우타" | "양타";
 export default function Batter({ battingSide }: { battingSide: BattingSide }) {
   return battingSide === "우타" ? (
-    <div css={side.single}>
+    <SideSingle>
       <RightBatter />
-    </div>
+    </SideSingle>
   ) : battingSide === "좌타" ? (
-    <div css={side.single}>
+    <SideSingle>
       <LeftBatter />
-    </div>
+    </SideSingle>
   ) : (
-    <div css={side.double}>
+    <SideDouble>
       <LeftBatter />
       <RightBatter />
-    </div>
+    </SideDouble>
   );
 }
--- src/components/achievement/pitcher.tsx
+++ src/components/achievement/pitcher.tsx
@@ -1,7 +1,7 @@
 import { ReactComponent as RightPitcher } from "./icons/rightPitcher.svg";
 import { ReactComponent as LeftPitcher } from "./icons/leftPitcher.svg";
-import { side } from "./achivement.style";
-/** @jsxImportSource @emotion/react */
+import { SideSingle, SideDouble } from "./batter";
+
 type PitchingSide = "좌투" | "우투" | "양투";
 export default function Pitcher({
   pitchingSide,
@@ -9,12 +9,17 @@ export default function Pitcher({
   pitchingSide: PitchingSide;
 }) {
   return pitchingSide === "좌투" ? (
-    <LeftPitcher />
+    <SideSingle>
+      <LeftPitcher />
+    </SideSingle>
   ) : pitchingSide === "우투" ? (
-    <RightPitcher />
+    <SideSingle>
+      <RightPitcher />
+    </SideSingle>
   ) : (
-    <div css={side.double}>
-      <LeftPitcher /> <RightPitcher />
-    </div>
+    <SideDouble>
+      <LeftPitcher />
+      <RightPitcher />
+    </SideDouble>
   );
 }
