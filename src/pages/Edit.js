524c2c6777735cb2a9b634341a6a30f165eed928
goeunleee <goeun3501@gmail.com>
2024-10-31 17:30:15 +0900

    [style]sidebar 레이아웃 수정

--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import { collection, getDocs } from "firebase/firestore";
-
 import CardBox from "../components/card/CardBox";
 import CardListBox from "../components/card/CardListBox";
 import Sidebar from "../components/edit/Sidebar";
@@ -12,12 +11,8 @@ const Edit = () => {
   const [scale, setScale] = useState(20);
   const [tab, setTab] = React.useState("1");
 
-  const handleTab = (newValue) => {
-    setTab(newValue);
-  };
-  const handleScaleChange = (event, newValue) => {
-    setScale(newValue);
-  };
+  const handleTab = (newValue) => setTab(newValue);
+  const handleScaleChange = (event, newValue) => setScale(newValue);
 
   useEffect(() => {
     const fetchFirestoreData = async () => {
@@ -28,32 +23,12 @@ const Edit = () => {
           id: doc.id,
         }));
         setCardList(cardData);
-
-        if (cardData.length > 0) {
-          setCard(cardData[0]);
-        }
+        if (cardData.length > 0) setCard(cardData[0]);
       } catch (error) {
         console.error("Error fetching Firestore data:", error);
       }
     };
 
-    const flattenObjectToString = (obj) => {
-      if (!obj || typeof obj !== "object") {
-        return ""; // type narrowing
-      }
-
-      return Object.entries(obj)
-        .map(([key, value]) => {
-          if (typeof value === "object" && value !== null) {
-            return `${key}: { ${flattenObjectToString(value)} }`;
-          }
-          return `${key}: ${String(value)}`;
-        })
-        .join(", ");
-    };
-
-    const flattenedString = flattenObjectToString(card);
-    console.log(flattenedString);
     fetchFirestoreData();
   }, []);
 
@@ -61,8 +36,9 @@ const Edit = () => {
     <div
       style={{
         display: "flex",
-        height: "100%",
-        flexGrow: 1,
+        maxHeight: "100%",
+        minHeight: "100vh",
+        overflow: "hidden",
       }}
     >
       <Sidebar card={card} setCard={setCard} tab={tab} handleTab={handleTab} />
@@ -71,7 +47,6 @@ const Edit = () => {
           flex: 1,
           display: "flex",
           flexDirection: "column",
-          height: "100%",
         }}
       >
         {cardList && (
